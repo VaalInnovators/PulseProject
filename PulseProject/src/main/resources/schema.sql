695ee30667ce9ad828ffdfae71e06ceb2c60675e
Ferry Tsolanku <ferrytsolanku@gmail.com>
2021-03-13 19:13:17 +0200

    Fixed schema query. FT

--- PulseProject/src/main/resources/schema.sql
+++ PulseProject/src/main/resources/schema.sql
@@ -1,18 +1,18 @@
 Create Table Employee
 (
-    id int;
-    Employee_Name varchar(50);
-    Employee_Surname varchar(50);
-    Employee_Type_No int;
-    Email_Address varchar(50);
-    Password varchar(50);
-    Employee_Date varchar(50);
-    System_Date varchar(50);
-    Phone_Number varchar(50);
-    Street_Address varchar(50);
-    Street_Name varchar(50);
-    Suburb varchar(50);
-    City varchar(50);
-    ZIp_Code int;
-    Entity_No int;
+    id int,
+    Employee_Name varchar(50),
+    Employee_Surname varchar(50),
+    Employee_Type_No int,
+    Email_Address varchar(50),
+    Password varchar(50),
+    Employee_Date varchar(50),
+    System_Date varchar(50),
+    Phone_Number varchar(50),
+    Street_Address varchar(50),
+    Street_Name varchar(50),
+    Suburb varchar(50),
+    City varchar(50),
+    ZIp_Code int,
+    Entity_No int
 )
