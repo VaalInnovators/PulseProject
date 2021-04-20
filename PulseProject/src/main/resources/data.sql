53d9f25420fb9844dafb5d94c0e9698ca502e76b
FerryTsolanku <ferrytsolanku@gmail.com>
2021-04-20 11:30:54 +0200

    Operational Structure Repo, Class and Controller. MM

--- PulseProject/src/main/resources/data.sql
+++ PulseProject/src/main/resources/data.sql
@@ -18,3 +18,14 @@ Select 'Ferry',
 
 insert into GOVERNANCE_STRUCTURE 
 Values(1,'Counsil','Yes','Yes','Ferry','Done','Zeph','2021-03-16','Meet requirements','2021-03-29');
+
+INSERT INTO FINANCIAL_OPERATION (
+financial_operation_description,
+financial_operation_financed,
+financial_operation_Responsible_Official,
+financial_operation_comments,
+financial_operation_action_who,
+financial_operation_action_when,
+financial_operation_action_output,
+financial_operation_date)
+VALUES ('Equitable share (figure) received as scheduled','Yes','No','Ferry','Done','Zeph','2021-03-16','Meet requirements','2021-03-29');
